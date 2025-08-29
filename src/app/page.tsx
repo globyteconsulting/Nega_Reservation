2eb0cc43d9b610cc1c0814af98d96ef20a409222
Befkadu Gebru <befkadugebru@Befkadus-MacBook-Air.local>
2025-08-28 22:17:23 -0400

    added the new nega code from Hiruy and added a Twilio integration

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ButcheryApp from '@/components/ButcheryApp';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background font-sans p-4 flex flex-col items-center">
-      <ButcheryApp />
-    </main>
-  );
-}
